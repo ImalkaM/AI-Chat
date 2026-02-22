f1970d7a2e3012b8908abbeef0fa4f8f22253aed
Imalka Muthukumara <imalka.muthukumara@gmail.com>
2026-02-22 19:26:16 +0100

    Initial Commit

---
+++ AI Chat/AI_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_ChatApp.swift
+//  AI Chat
+//
+//  Created by Imalka Muthukumara on 2026-02-22.
+//
+
+import SwiftUI
+
+@main
+struct AI_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI ChatTests/AI_ChatTests.swift
@@ -0,0 +1,16 @@
+//
+//  AI_ChatTests.swift
+//  AI ChatTests
+//
+//  Created by Imalka Muthukumara on 2026-02-22.
+//
+
+import Testing
+
+struct AI_ChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
