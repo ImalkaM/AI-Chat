f43c9370667c92ff0db8dbcf64d926fea6caab4e
Imalka Muthukumara <imalka.muthukumara@gmail.com>
2026-03-10 10:48:39 +0100

    [Feature] Add AppState

---
+++ AI Chat/Core/Onbaording/CompletedView/OnBoardingCompletedView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingCompletedView.swift
+//  AI Chat
+//
+//  Created by Imalka Muthukumara on 2026-03-07.
+//
+
+import SwiftUI
+
+struct OnBoardingCompletedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoardingCompletedView()
+}
---
+++ AI Chat/Core/Settings/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  AI Chat
+//
+//  Created by Imalka Muthukumara on 2026-03-10.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
