e54efcc14ca0cc6bd6d4647fd50580b586b358a1
Imalka Muthukumara <imalka.muthukumara@gmail.com>
2026-03-11 15:03:45 +0100

    [Feature] Add WelcomeView UI

---
+++ AI Chat/Core/Components/Images/ImageLoaderView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageLoaderView.swift
+//  AI Chat
+//
+//  Created by Imalka Muthukumara on 2026-03-11.
+//
+
+import SwiftUI
+
+struct ImageLoaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageLoaderView()
+}
