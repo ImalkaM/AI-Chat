0c52a2e7ba309a91a6172d4cf99cfbbf81e89f2c
Imalka Muthukumara <imalka.muthukumara@gmail.com>
2026-05-10 11:48:26 +0200

    [Feature]  CustomlistCellView

---
+++ AI Chat/Core/Components/ViewModifiers/ButtonViewModfiers.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonViewModfiers.swift
+//  AI Chat
+//
+//  Created by Imalka Muthukumara on 2026-05-05.
+//
+
+import SwiftUI
+
+struct ButtonViewModfiers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonViewModfiers()
+}
